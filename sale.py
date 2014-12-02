4159a7c5e9617ba12e609a49a0c42432c70b5027
jmartin <jmartin@zikzakmedia.com>
2014-12-02 10:27:02 +0100

    Added new criteria for comparing product and manufacturer together

--- sale.py
+++ sale.py
@@ -3,7 +3,7 @@
 # copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
-from trytond.pyson import Eval
+from trytond.pyson import Eval, Equal, In, Not
 
 __all__ = ['SaleRuleCondition']
 __metaclass__ = PoolMeta
@@ -13,8 +13,10 @@ class SaleRuleCondition:
     __name__ = 'sale.rule.condition'
     manufacturer = fields.Many2One('party.party', 'Manufacturer',
         states={
-            'required': Eval('criteria') == 'manufacturer',
-            'invisible': Eval('criteria') != 'manufacturer',
+            'required': In(Eval('criteria'),
+                ['manufacturer', 'product_manufacturer']),
+            'invisible': Not(In(Eval('criteria'),
+                    ['manufacturer', 'product_manufacturer'])),
             },
         domain=[
             ('manufacturer', '=', True),
@@ -24,9 +26,33 @@ class SaleRuleCondition:
     @classmethod
     def __setup__(cls):
         super(SaleRuleCondition, cls).__setup__()
-        criteria = ('manufacturer', 'Total Product Manufacturer Quantity')
+        criteria = ('manufacturer', 'Total of Products of a Manufacturer')
         if criteria not in cls.criteria.selection:
             cls.criteria.selection.append(criteria)
+        criteria = ('product_manufacturer', 'Total of a Product of a '
+            'Manufacturer')
+        if criteria not in cls.criteria.selection:
+            cls.criteria.selection.append(criteria)
+        required = Equal(Eval('criteria'), 'product_manufacturer')
+        previous_required = cls.product.states['required']
+        if previous_required:
+            required |= previous_required
+        invisible = Not(Equal(Eval('criteria'), 'product_manufacturer'))
+        previous_invisible = cls.product.states['invisible']
+        if previous_invisible:
+            invisible &= previous_invisible
+        cls.product.states.update({
+                'required': required,
+                'invisible': invisible,
+                })
 
     def evaluate_manufacturer(self, sale):
         return self.evaluate_sum(sale)
+
+    def evaluate_product_manufacturer(self, sale):
+        quantity = sum([l.quantity for l in sale.lines
+                if (getattr(l, 'product') == getattr(self, 'product') and
+                    getattr(l, 'manufacturer') == getattr(self, 'manufacturer')
+                    )
+                ])
+        return self.apply_comparison(quantity)
