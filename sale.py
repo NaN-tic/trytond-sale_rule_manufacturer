39e2370312db43f467ecc3f32c46d1a7a6ac2c78
jmartin <jmartin@zikzakmedia.com>
2014-12-11 09:34:58 +0100

    Add rule to evaluate sales by category of products and manufacturer

--- sale.py
+++ sale.py
@@ -2,7 +2,7 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval, Equal, In, Not
 
 __all__ = ['SaleRuleCondition']
@@ -14,9 +14,11 @@ class SaleRuleCondition:
     manufacturer = fields.Many2One('party.party', 'Manufacturer',
         states={
             'required': In(Eval('criteria'),
-                ['manufacturer', 'product_manufacturer']),
+                ['manufacturer', 'product_manufacturer',
+                    'category_manufacturer']),
             'invisible': Not(In(Eval('criteria'),
-                    ['manufacturer', 'product_manufacturer'])),
+                    ['manufacturer', 'product_manufacturer',
+                        'category_manufacturer'])),
             },
         domain=[
             ('manufacturer', '=', True),
@@ -47,12 +49,42 @@ class SaleRuleCondition:
                 'invisible': invisible,
                 })
 
+        criteria = ('category_manufacturer', 'Total Amount of a Category '
+            'of Product of a Manufacturer')
+        if criteria not in cls.criteria.selection:
+            cls.criteria.selection.append(criteria)
+        required = Equal(Eval('criteria'), 'category_manufacturer')
+        previous_required = cls.category.states['required']
+        if previous_required:
+            required |= previous_required
+        invisible = Not(Equal(Eval('criteria'), 'category_manufacturer'))
+        previous_invisible = cls.category.states['invisible']
+        if previous_invisible:
+            invisible &= previous_invisible
+        cls.category.states.update({
+                'required': required,
+                'invisible': invisible,
+                })
+
     def evaluate_manufacturer(self, sale):
         return self.evaluate_sum(sale)
 
     def evaluate_product_manufacturer(self, sale):
         quantity = sum([l.quantity for l in sale.lines
                 if (getattr(l, 'product') == getattr(self, 'product') and
+                    getattr(l, 'manufacturer') == getattr(self, 'manufacturer')
+                    )
+                ])
+        return self.apply_comparison(quantity)
+
+    def evaluate_category_manufacturer(self, sale):
+        pool = Pool()
+        Template = pool.get('product.template')
+        templates = Template.search([
+                (self.criteria, '=', getattr(self, self.criteria)),
+                ])
+        quantity = sum([l.quantity for l in sale.lines
+                if (getattr(l, 'category') in templates and
                     getattr(l, 'manufacturer') == getattr(self, 'manufacturer')
                     )
                 ])
