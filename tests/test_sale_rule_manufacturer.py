0974da8dc52ebb2617866c7d25e77c8fc2c375c1
resteve <resteve@zikzakmedia.com>
2015-12-01 11:22:03 +0100

    Tests

--- tests/test_sale_rule_manufacturer.py
+++ tests/test_sale_rule_manufacturer.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the sale_rule_manufacturer module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleRuleManufacturerTestCase(unittest.TestCase):
-    'Test Sale Rule module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_rule_manufacturer')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_rule_manufacturer')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class SaleRuleManufacturerTestCase(ModuleTestCase):
+    'Test Sale Rule Manufacturer module'
+    module = 'sale_rule_manufacturer'
 
 
 def suite():
